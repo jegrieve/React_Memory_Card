78fdb0657cab616b729c39f076738ed1a36be071
James Grieve <pocojames@hotmail.com>
2021-02-04 13:34:54 -0800

    game logic complete

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -3,23 +3,27 @@ import Card from './Card';
 import Navbar from './Navbar.js';
 const CardContainer = (props) => {
   const [currentPicks, setCurrentPicks] = useState([]);
-  const [highScore, setHighScore] = useState(0);
   const [newScore, setNewScore] = useState(0);
-  const shuffledCards = props.shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  let shuffledCards = props.shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   const getCardId = (id) => {
     setCurrentPicks([...currentPicks, id]);
-    console.log(currentPicks);
   };
-  //So, I add an event listener on each card that will return its ID
-  //I will take this id check the array and if its not picked then I will update the score Note: highscore only updated when lose/win
-  //Keep going until:
-  //1. Array.length === 10, u win, replay
-  //2. You pcik an id already in the array, you lose but try again
-  //3. On all tries you keep the navbar with the highscore but u update own cardcontainer score.
+
+  useEffect(() => {
+    if (currentPicks.length === 0) return;
+    let copiedPicks = Array.from(currentPicks);
+    let latestNum = copiedPicks.pop();
+    if (copiedPicks.indexOf(latestNum) === -1) {
+      setNewScore(newScore + 1);
+    } else {
+      setNewScore(0);
+    }
+  }, [currentPicks]);
+
   return (
     <div>
-      <Navbar highScore={highScore} newScore={newScore} />
+      <Navbar newScore={newScore} />
       {shuffledCards.map((id, i) => {
         return (
           <div key={i}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 const Navbar = (props) => {
+  const [highScore, setHighScore] = useState(0);
+  const [placeHolderScore, setPlaceHolderScore] = useState(0);
+
+  useEffect(() => {
+    if (props.newScore > placeHolderScore) {
+      setPlaceHolderScore(props.newScore);
+    } else if (placeHolderScore > highScore && props.newScore === 0) {
+      setHighScore(placeHolderScore);
+    }
+  });
+
   return (
     <div>
       <div>
         <span>Score: {props.newScore} </span>
-        <span>High Score: {props.highScore}</span>
+        <span>High Score: {highScore}</span>
       </div>
     </div>
   );
